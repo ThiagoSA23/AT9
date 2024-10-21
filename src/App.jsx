36ebc67f488f7715b16d9d656e5f5bdb0926dfe5
ThiagoSA23 <thiago.azevedo@iesb.edu.br>
2024-10-21 00:04:37 -0300

    cmt

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ function App () {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
